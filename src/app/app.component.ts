c2ee9a69922b796a335597484b742a6a73f9b73f
Developerani <developer.aani@gmail.com>
2019-12-19 16:23:17 +0530

    Barcode

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+
+  ngOnInit() {
+    this.data();
+  }
+  name = 'Angular 6';
+
+  PrintSerials = []
+  
+ data(){
+  let data = {
+    SerialId: 12345,
+    Name: 'Aniket Sanjay Giram',
+    quantity:100,
+    batchno:1
+  };
+   for (let index = 0; index < 10; index++) {
+    this.PrintSerials.push(data);
+   }
+
+   for (let index = 0; index < 10; index++) {
+    console.log(this.PrintSerials[index]);
+  }
+ }
+
+
 }
